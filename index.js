2872952089a0fd348a442bcb7441127591707935
hachi8833 <hachi8833@gmail.com>
2021-07-25 09:12:45 +0900

    Implement letters()

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = Phrase;
 
-// reverse()メソッドを追加してすべてのStringで使えるようにする
+// reverse()メソッドを追加してすべてのStringで使えるようにす
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
@@ -12,6 +12,19 @@ function Phrase(content) {
   // パリンドロームのテスト用に変換した訳文を返す
   this.processedContent = function processedContent() {
     return this.content.toLowerCase();
+  }
+
+  // コンテンツの文字だけを返す
+  // 利用例:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    let theLetters = [];
+    for (let i = 0; i < this.content.length; i++) {
+      if (this.content.charAt(i).match(/[a-zA-Z]/)) {
+        theLetters.push(this.content.charAt(i));
+      }
+    }
+    return theLetters.join("");
   }
 
   // パリンドロームならtrueを、違うならfalseを返す
